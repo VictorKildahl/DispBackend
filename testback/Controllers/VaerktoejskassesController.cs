07c6c24184a3e8dab3c7f03a19979bc5f109e43b
Lenni <lena-bach@hotmail.com>
2021-03-18 11:29:28 +0100

    Add project files.

---
+++ testback/Controllers/VaerktoejskassesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using F20ITONK.ASPNETCore.MicroService.ClassLib.Models;
+
+namespace backend
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaerktoejskassesController : ControllerBase
+    {
+        private readonly HaandvaerkerContext _context;
+
+        public VaerktoejskassesController(HaandvaerkerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vaerktoejskasses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vaerktoejskasse>>> GetVaerktoejskasse()
+        {
+            return await _context.Vaerktoejskasse.ToListAsync();
+        }
+
+        // GET: api/Vaerktoejskasses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vaerktoejskasse>> GetVaerktoejskasse(int id)
+        {
+            var vaerktoejskasse = await _context.Vaerktoejskasse.FindAsync(id);
+
+            if (vaerktoejskasse == null)
+            {
+                return NotFound();
+            }
+
+            return vaerktoejskasse;
+        }
+
+        // PUT: api/Vaerktoejskasses/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVaerktoejskasse(int id, Vaerktoejskasse vaerktoejskasse)
+        {
+            if (id != vaerktoejskasse.VTKId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vaerktoejskasse).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VaerktoejskasseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vaerktoejskasses
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Vaerktoejskasse>> PostVaerktoejskasse(Vaerktoejskasse vaerktoejskasse)
+        {
+            _context.Vaerktoejskasse.Add(vaerktoejskasse);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVaerktoejskasse", new { id = vaerktoejskasse.VTKId }, vaerktoejskasse);
+        }
+
+        // DELETE: api/Vaerktoejskasses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Vaerktoejskasse>> DeleteVaerktoejskasse(int id)
+        {
+            var vaerktoejskasse = await _context.Vaerktoejskasse.FindAsync(id);
+            if (vaerktoejskasse == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vaerktoejskasse.Remove(vaerktoejskasse);
+            await _context.SaveChangesAsync();
+
+            return vaerktoejskasse;
+        }
+
+        private bool VaerktoejskasseExists(int id)
+        {
+            return _context.Vaerktoejskasse.Any(e => e.VTKId == id);
+        }
+    }
+}
---
+++ testback/Data/HaandvaerkerContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using F20ITONK.ASPNETCore.MicroService.ClassLib.Models;
+
+public class HaandvaerkerContext : DbContext
+{
+    public HaandvaerkerContext(DbContextOptions<HaandvaerkerContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Haandvaerker> Haandvaerker { get; set; }
+
+    public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Vaerktoej> Vaerktoej { get; set; }
+
+    public DbSet<F20ITONK.ASPNETCore.MicroService.ClassLib.Models.Vaerktoejskasse> Vaerktoejskasse { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Haandvaerker
+        modelBuilder.Entity<Haandvaerker>().HasKey(s => s.HaandvaerkerId);
+
+        //Vaerktoejskasse
+        modelBuilder.Entity<Vaerktoejskasse>().HasKey(p => p.VTKId);
+
+        modelBuilder.Entity<Haandvaerker>()
+            .HasMany<Vaerktoejskasse>(s => s.Vaerktoejskasse)
+            .WithOne(vt => vt.EjesAfNavigation)
+            .HasForeignKey(s => s.VTKEjesAf);
+
+        modelBuilder.Entity<Vaerktoejskasse>()
+            .HasMany<Vaerktoej>(s => s.Vaerktoej)
+            .WithOne(s => s.LiggerIvtkNavigation)
+            .HasForeignKey(s => s.LiggerIvtk);
+
+    }
+
+}
